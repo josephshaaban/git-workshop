2ff54aef3d349341ac9ec20b2485d2eb0e4909c2
Youssef Shaaban <y.shaaban@lit-co.net>
2019-11-16 09:11:21 +0200

    initial commit

---
+++ goodbay.py
@@ -0,0 +1,3 @@
+if __name__ = "__main__":
+    print("You've worked hard, Thankyou!")
+    print("Goodbay!!")
---
+++ hello.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("Hello!!")
+    print("Are you ready to work with git?")
+    print("Lets go")
+    print()
+    print("First file in our repo")
+    print("You can start developing..")
